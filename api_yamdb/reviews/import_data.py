05ca031785f256eb379b36c66d61aa461d1173ea
N0len-sasha <sasaplatosin@gmail.com>
2023-11-07 20:39:20 +0300

    added new import methods

--- api_yamdb/reviews/import_data.py
+++ api_yamdb/reviews/import_data.py
@@ -1,5 +1,6 @@
 import csv
-from reviews.models import Genre, Category
+from reviews.models import Genre, Category, Title
+from django.db import connection
 
 
 def import_data_from_csv():
@@ -15,4 +16,60 @@ def import_data_from_csv():
               encoding='utf-8') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            Category.objects.create(name=row['name'], slug=row['slug'])
+            Category.objects.get_or_create(name=row['name'], slug=row['slug'])
+
+    with open('static/data/titles.csv',
+              newline='',
+              encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            Title.objects.get_or_create(
+                name=row['name'],
+                year=int(row['year']),
+                category=Category.objects.get(id=int(row['category'])),
+            )
+
+
+    with open('static/data/genre_title.csv',
+              newline='',
+              encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            title_id = int(row['title_id'])
+            genre_id = int(row['genre_id'])
+            try:
+                with connection.cursor() as cursor:
+                    cursor.execute(
+                        "INSERT INTO reviews_title_genre (title_id,genre_id) VALUES (%s, %s)",
+                        [title_id, genre_id]
+                    )
+
+            except Exception as e:
+                print(f"Error: {e}")
+
+    # with open('static/data/comments.csv',
+    #           newline='',
+    #           encoding='utf-8') as csvfile:
+    #     reader = csv.DictReader(csvfile)
+    #     # for row in reader:
+    #     #     Comment.objects.get_or_create(
+    #     #         #атрибуты комментария
+    #     #     )
+
+    # with open('static/data/review.csv',
+    #           newline='',
+    #           encoding='utf-8') as csvfile:
+    #     reader = csv.DictReader(csvfile)
+    #     # for row in reader:
+    #     #     Review.objects.get_or_create(
+    #     #         #атрибуты ревью
+    #     #     )
+
+    # with open('static/data/users.csv',
+    #           newline='',
+    #           encoding='utf-8') as csvfile:
+    #     reader = csv.DictReader(csvfile)
+    #     # for row in reader:
+    #     #     Title.objects.get_or_create(
+    #     #         #атрибуты юзера
+    #     #     )
