88c7cc84eee1f8c0e0c9c102b71b971179262072
N0len-sasha <sasaplatosin@gmail.com>
2023-11-13 00:32:07 +0300

    fixed filters

--- api_yamdb/api/filters.py
+++ api_yamdb/api/filters.py
@@ -5,10 +5,9 @@ from reviews.models import Title
 
 class CustomFilter(django_filters.FilterSet):
     genre = django_filters.CharFilter(field_name='genre__slug',
-                                      method='filter_by_genre') # Методы тут не нужны, нужно просто прописать lookup_expr и в категории.
+                                      lookup_expr='exact')
     category = django_filters.CharFilter(field_name='category__slug',
-                                         method='filter_by_category')
-    year = django_filters.NumberFilter(field_name='year', lookup_expr='exact') # Избыточно тут указывать год, достаточно указать его в мете.
+                                         lookup_expr='exact')
     name = django_filters.CharFilter(field_name='name', lookup_expr='exact')
 
     class Meta:
